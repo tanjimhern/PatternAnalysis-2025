8ad236c4275af3511fb2798af3b0a76982a5cc2c
tanjimhern <143367483+tanjimhern@users.noreply.github.com>
2025-10-09 22:31:23 +1000

    No longer need this function and we can remove to keep code clean

--- recognition/ConvNeXt_Alzheimers_49315271/dataset.py
+++ recognition/ConvNeXt_Alzheimers_49315271/dataset.py
@@ -58,68 +58,6 @@ class ADNIDataset(Dataset):
         
         return image, label
 
-
-def get_data_loaders(data_path, batch_size=32, num_workers=2):
-    """
-    Create train and test data loaders with appropriate transforms
-    
-    Args:
-        data_path: Path to AD_NC folder
-        batch_size: Batch size for training
-        num_workers: Number of workers for data loading
-    
-    Returns:
-        train_loader, test_loader
-    """
-    # Data augmentation for training
-    train_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomRotation(degrees=20),  # Increase from 10
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),  # Add translation
-        transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),  # Add random crop
-        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2),  # Stronger
-        transforms.RandomGrayscale(p=0.1),  # Occasionally convert to grayscale
-        transforms.ToTensor(),
-        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)),  # Add blur
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    
-    # No augmentation for testing
-    test_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    
-    # Create datasets
-    train_dataset = ADNIDataset(
-        root_dir=os.path.join(data_path, 'train'),
-        transform=train_transform
-    )
-    
-    test_dataset = ADNIDataset(
-        root_dir=os.path.join(data_path, 'test'),
-        transform=test_transform
-    )
-    
-    # Create data loaders
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers
-    )
-    
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers
-    )
-    
-    return train_loader, test_loader
-
 def get_data_loaders_proper_split(data_path, batch_size=32, val_split=0.2, random_seed=42, num_workers=2):
     """
     Create train, validation, and test loaders with proper splits
